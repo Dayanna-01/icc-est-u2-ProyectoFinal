328749bb2f0febc8bc267eb28303d3e34b1bab55
Dayanna Chacha <jchachav@est.ups.edu.ec>
2026-01-31 23:28:37 -0500

    Clase Tiempo de ejecucion

--- src/utils/TiempoEjecucion.java
+++ src/utils/TiempoEjecucion.java
@@ -5,12 +5,10 @@ import java.io.IOException;
 
 public class TiempoEjecucion {
 
-    // Devuelve el sistema en nanosegundos
     public static long iniciar(){
         return System.nanoTime();
     }
 
-    // Calcula cuanto tiempo pasa desde que inicio
     public static long finalizar(long inicio){
         return System.nanoTime() - inicio;
     }
